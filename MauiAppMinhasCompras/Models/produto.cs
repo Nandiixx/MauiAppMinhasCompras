8e43752c9968e595e1cdd5f829393bccd64c9779
Nandiixx <87515323+Nandiixx@users.noreply.github.com>
2025-03-04 17:04:39 -0300

    Att

--- MauiAppMinhasCompras/Models/produto.cs
+++ MauiAppMinhasCompras/Models/produto.cs
@@ -9,7 +9,12 @@ using SQLite;
 
 namespace MauiAppMinhasCompras.Models
 {
-    class produto
+    class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public double Preco { get; set; }
     }
 }
